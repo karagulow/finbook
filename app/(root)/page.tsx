9e48b39b8e5fe38ca39caaf928ba548b93220f37
Albert Karagulov <albert.karagulov@mail.ru>
2025-09-15 10:23:18 +0300

    feat: implement user balance component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,13 @@
+import { Button } from '@/shared/ui';
+import { Balance } from '@/entities/balance';
+
 export default function Home() {
-	return <div>Hello World</div>;
+	return (
+		<div className='flex flex-col gap-5 sm:gap-[30px]'>
+			<div className='flex flex-col items-start gap-2.5 sm:flex-row sm:justify-between sm:items-center'>
+				<Balance />
+				<Button className='w-full sm:w-auto'>Добавить транзакцию</Button>
+			</div>
+		</div>
+	);
 }
---
+++ src/entities/balance/ui/balance.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import React, { useEffect } from 'react';
+import { useBalance } from '../model/use-balance';
+
+export const Balance: React.FC = () => {
+	const { total, fetchBalance, currencyCode, currencySymbol } = useBalance();
+
+	useEffect(() => {
+		fetchBalance();
+	}, [fetchBalance]);
+
+	return (
+		<div className='font-medium text-[24px] text-[var(--foreground-secondary)]'>
+			Баланс:{' '}
+			<span className='text-[var(--foreground-primary)]'>
+				{total.toLocaleString('ru-RU', { minimumFractionDigits: 2 })}{' '}
+				{currencySymbol || currencyCode}
+			</span>
+		</div>
+	);
+};
