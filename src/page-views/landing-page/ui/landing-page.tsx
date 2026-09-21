9c9302a452ba83c534b205640001576f2bac27f6
Albert Karagulov <albert.karagulov@mail.ru>
2026-09-21 11:50:37 +0300

    feat(landing): add FAQ section

--- src/page-views/landing-page/ui/landing-page.tsx
+++ src/page-views/landing-page/ui/landing-page.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { LandingHero } from './hero';
+import { LandingFaq } from './faq';
 
 export const LandingPage: React.FC = () => {
 	return (
 		<div className='flex flex-col'>
 			<LandingHero />
+			<LandingFaq />
 		</div>
 	);
 };
